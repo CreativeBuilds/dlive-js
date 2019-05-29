19ccbe52f5fb44856b21278f89d2338d4ad2cc37
CreativeBuilds <gaminggonecreative@gmail.com>
2019-05-28 21:36:33 -0400

    Fixed ws for browser

--- src/helpers/rxChat.js
+++ src/helpers/rxChat.js
@@ -1,48 +1,146 @@
 const makeWebSocketObservable = require('rxjs-websockets').default;
-const { switchMap, retryWhen, delay } = require('rxjs/operators');
+const { switchMap, retryWhen, delay, filter } = require('rxjs/operators');
+const { BehaviorSubject } = require('rxjs');
 const { QueueingSubject } = require('queueing-subject');
-const WebSocket = require('ws');
+const WebSocket2 = require('ws');
 
 // Takes a blockchainUsername from the linoBlockchain NOT a dlive username
 const makeSocket = blockchainUsername => {
-  const input$ = new QueueingSubject();
+  if (WebSocket) {
+    // User is in browser!
+    const socket$ = new BehaviorSubject(null);
 
-  const socket$ = makeWebSocketObservable('wss://graphigostream.prd.dlive.tv', {
-    makeWebSocket: (uri, protocols) => {
-      let ws = new WebSocket(uri, protocols);
-      ws.on('open', function() {
-        ws.send(
-          JSON.stringify({
-            type: 'connection_init',
-            payload: {}
-          })
-        );
-        ws.send(
-          JSON.stringify({
-            id: '1',
-            type: 'start',
-            payload: {
-              variables: {
-                streamer: blockchainUsername
-              },
-              extensions: {},
-              operationName: 'StreamMessageSubscription',
-              query:
-                'subscription StreamMessageSubscription($streamer: String!) {\n  streamMessageReceived(streamer: $streamer) {\n    type\n    ... on ChatGift {\n      id\n      gift\n      amount\n      recentCount\n      expireDuration\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatHost {\n      id\n      viewer\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatSubscription {\n      id\n      month\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatChangeMode {\n      mode\n    }\n    ... on ChatText {\n      id\n      content\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatFollow {\n      id\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatDelete {\n      ids\n    }\n    ... on ChatBan {\n      id\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatModerator {\n      id\n      ...VStreamChatSenderInfoFrag\n      add\n    }\n    ... on ChatEmoteAdd {\n      id\n      ...VStreamChatSenderInfoFrag\n      emote\n    }\n  }\n}\n\nfragment VStreamChatSenderInfoFrag on SenderInfo {\n  subscribing\n  role\n  roomRole\n  sender {\n    id\n    username\n    displayname\n    avatar\n    partnerStatus\n  }\n}\n'
+    const messages$ = new BehaviorSubject(null).pipe(filter(x => !!x));
+
+    socket$.pipe(filter(x => !!x)).subscribe(ws => {
+      ws.send(
+        JSON.stringify({
+          type: 'connection_init',
+          payload: {}
+        })
+      );
+      ws.send(
+        JSON.stringify({
+          id: '1',
+          type: 'start',
+          payload: {
+            variables: {
+              streamer: blockchainUsername
+            },
+            extensions: {},
+            operationName: 'StreamMessageSubscription',
+            query:
+              'subscription StreamMessageSubscription($streamer: String!) {\n  streamMessageReceived(streamer: $streamer) {\n    type\n    ... on ChatGift {\n      id\n      gift\n      amount\n      recentCount\n      expireDuration\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatHost {\n      id\n      viewer\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatSubscription {\n      id\n      month\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatChangeMode {\n      mode\n    }\n    ... on ChatText {\n      id\n      content\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatFollow {\n      id\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatDelete {\n      ids\n    }\n    ... on ChatBan {\n      id\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatModerator {\n      id\n      ...VStreamChatSenderInfoFrag\n      add\n    }\n    ... on ChatEmoteAdd {\n      id\n      ...VStreamChatSenderInfoFrag\n      emote\n    }\n  }\n}\n\nfragment VStreamChatSenderInfoFrag on SenderInfo {\n  subscribing\n  role\n  roomRole\n  sender {\n    id\n    username\n    displayname\n    avatar\n    partnerStatus\n  }\n}\n'
+          }
+        })
+      );
+      ws.send(
+        JSON.stringify({
+          id: '2',
+          type: 'start',
+          payload: {
+            variables: {
+              streamer: blockchainUsername
+            },
+            extensions: {},
+            operationName: 'TreasureChestMessageReceived',
+            query: `subscription TreasureChestMessageReceived($streamer: String!) {
+          treasureChestMessageReceived(streamer: $streamer) {
+            type
+            ... on TreasureChestGiveawayEnded {
+              type
+              __typename
             }
-          })
-        );
-        ws.send(
-          JSON.stringify({
-            id: '2',
-            type: 'start',
-            payload: {
-              variables: {
-                streamer: blockchainUsername
-              },
-              extensions: {},
-              operationName: 'TreasureChestMessageReceived',
-              query: `subscription TreasureChestMessageReceived($streamer: String!) {
+            ... on TreasureChestValueExpired {
+              type
+              expireAt
+              value
+              __typename
+            }
+            ... on TreasureChestGiveawayStarted {
+              type
+              endTime
+              pricePool
+              durationInSeconds
+              __typename
+            }
+            ... on TreasureChestReadyToCollect {
+              type
+              __typename
+            }
+            ... on TreasureChestValueUpdated {
+              type
+              value
+              __typename
+            }
+            __typename
+          }
+        }
+        `
+          }
+        })
+      );
+      ws.onmessage = msg => {
+        messages$.next(msg.data);
+      };
+    });
+
+    let startBrowser = (websocketServerLocation, protocols) => {
+      ws = new WebSocket(websocketServerLocation, protocols);
+      ws.onopen = function(e) {
+        socket$.next(ws);
+      };
+      ws.onclose = function(e) {
+        // Try to reconnect in 5 seconds
+        setTimeout(function() {
+          startBrowser(websocketServerLocation, protocols);
+        }, 5000);
+      };
+    };
+
+    startBrowser('wss://graphigostream.prd.dlive.tv', 'graphql-ws');
+    return messages$;
+  } else {
+    const input$ = new QueueingSubject();
+
+    const socket$ = makeWebSocketObservable(
+      'wss://graphigostream.prd.dlive.tv',
+      {
+        makeWebSocket: (uri, protocols) => {
+          let ws = new WebSocket2(uri, protocols);
+          ws.on('open', function() {
+            ws.send(
+              JSON.stringify({
+                type: 'connection_init',
+                payload: {}
+              })
+            );
+            ws.send(
+              JSON.stringify({
+                id: '1',
+                type: 'start',
+                payload: {
+                  variables: {
+                    streamer: blockchainUsername
+                  },
+                  extensions: {},
+                  operationName: 'StreamMessageSubscription',
+                  query:
+                    'subscription StreamMessageSubscription($streamer: String!) {\n  streamMessageReceived(streamer: $streamer) {\n    type\n    ... on ChatGift {\n      id\n      gift\n      amount\n      recentCount\n      expireDuration\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatHost {\n      id\n      viewer\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatSubscription {\n      id\n      month\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatChangeMode {\n      mode\n    }\n    ... on ChatText {\n      id\n      content\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatFollow {\n      id\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatDelete {\n      ids\n    }\n    ... on ChatBan {\n      id\n      ...VStreamChatSenderInfoFrag\n    }\n    ... on ChatModerator {\n      id\n      ...VStreamChatSenderInfoFrag\n      add\n    }\n    ... on ChatEmoteAdd {\n      id\n      ...VStreamChatSenderInfoFrag\n      emote\n    }\n  }\n}\n\nfragment VStreamChatSenderInfoFrag on SenderInfo {\n  subscribing\n  role\n  roomRole\n  sender {\n    id\n    username\n    displayname\n    avatar\n    partnerStatus\n  }\n}\n'
+                }
+              })
+            );
+            ws.send(
+              JSON.stringify({
+                id: '2',
+                type: 'start',
+                payload: {
+                  variables: {
+                    streamer: blockchainUsername
+                  },
+                  extensions: {},
+                  operationName: 'TreasureChestMessageReceived',
+                  query: `subscription TreasureChestMessageReceived($streamer: String!) {
                 treasureChestMessageReceived(streamer: $streamer) {
                   type
                   ... on TreasureChestGiveawayEnded {
@@ -75,23 +173,25 @@ const makeSocket = blockchainUsername => {
                 }
               }
               `
-            }
-          })
-        );
-      });
-      return ws;
-    },
-    protocols: ['graphql-ws']
-  });
+                }
+              })
+            );
+          });
+          return ws;
+        },
+        protocols: ['graphql-ws']
+      }
+    );
 
-  const messages$ = socket$.pipe(
-    // the observable produces a value once the websocket has been opened
-    switchMap(getResponses => {
-      return getResponses(input$);
-    }),
-    retryWhen(errors => errors.pipe(delay(1000)))
-  );
-  return messages$;
+    const messages$ = socket$.pipe(
+      // the observable produces a value once the websocket has been opened
+      switchMap(getResponses => {
+        return getResponses(input$);
+      }),
+      retryWhen(errors => errors.pipe(delay(1000)))
+    );
+    return messages$;
+  }
 };
 
 module.exports = makeSocket;
