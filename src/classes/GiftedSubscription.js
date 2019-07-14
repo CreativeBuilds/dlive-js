e6b4d3eb6dfa0c9e8e860e4f1bd1a69e15d1c02e
CreativeBuilds <gaminggonecreative@gmail.com>
2019-07-14 10:05:36 -0400

    Added count to giftedsubscription

--- src/classes/GiftedSubscription.js
+++ src/classes/GiftedSubscription.js
@@ -4,6 +4,7 @@ const Subscription = class extends Message {
   constructor(msg, streamerBlockchainUsername, permissionObj) {
     super(msg, streamerBlockchainUsername, permissionObj);
     this.receiver = msg.receiver;
+    this.count = msg.count;
     this.content = `${this.sender.dliveUsername} just sent ${
       this.receiver
     } a one-month subscription!`;
